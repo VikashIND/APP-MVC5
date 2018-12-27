b3cb4001ec0669098104fe49eb63e5328b508e90
vikashIND <rkvikashraj@gmail.com>
2018-12-27 20:48:47 +0530

    Add project files.

---
+++ MVC5-APP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5_APP.MvcApplication" Language="C#" %>
---
+++ MVC5-APP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_APP.Startup))]
+namespace MVC5_APP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
