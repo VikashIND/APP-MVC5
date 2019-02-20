dfbda3d47524a4e130159abf85cc465d5c5ad75a
vikashIND <rkvikashraj@gmail.com>
2019-02-20 23:49:37 +0530

    LogFilter created

--- MVC5-APP/App_Start/FilterConfig.cs
+++ MVC5-APP/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using MVC5_APP.Filters;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MVC5_APP
@@ -8,6 +9,7 @@ namespace MVC5_APP
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new LogFilterAttribute());
         }
     }
 }
---
+++ MVC5-APP/Filters/LogFilterAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC5_APP.Filters
+{
+    public class LogFilterAttribute:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Trace("OnActionExecuting", filterContext.RouteData);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Trace("OnActionExecuted", filterContext.RouteData);
+
+
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Trace("OnResultExecuted", filterContext.RouteData);
+
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Trace("OnResultExecuted", filterContext.RouteData);
+
+        }
+
+        private void Trace(string methodName, RouteData routeData)
+        {
+            var actonName = routeData.Values["Action"].ToString();
+            var controllerName = routeData.Values["Controller"].ToString();
+
+            HttpContext.Current.Response.Write($"MethodName={methodName}, Controller={controllerName}, Action={actonName}<hr>");
+        }
+    }
+}
